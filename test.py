ef51aaa921e48ad51003fd399b6b0cbce3bf326e
anna <anna.martins@ecam.fr>
2023-11-29 13:45:07 +0100

    annaagain

--- test.py
+++ test.py
@@ -1,2 +1,30 @@
-a=10
-b=11
+import random
+
+# Initialisation des cartes
+valeurs = ["7", "8", "9", "10", "Valet", "Dame", "Roi", "As"]
+couleurs = ["Coeur", "Carreau", "Trèfle", "Pique"]
+cartes =[(v, c) for v in valeurs for c in couleurs]
+
+# Mélange des cartes
+random.shuffle(cartes)
+
+
+# Distribution des cartes aux joueurs
+joueurs = [[], [], [], []]
+for i in range(len(cartes)):
+    carte = cartes[i]
+    joueurs[i % 4].append(carte)  # % permet d'obtenir le reste de la division i/4 donc (0,1,2,3, etc),  permet de distribuer les cartes alternativement
+                                          
+
+# Affichage des mains des joueurs
+for i in range(len(joueurs)):
+    numero_joueur = i + 1
+    representation_cartes = ""
+    for valeur, couleur in joueurs[i]:
+        representation_cartes += f'{valeur} de {couleur}, ' 
+    
+    representation_cartes = representation_cartes[:-2]     # Supprime les deux derniers caractères (virgule à la fin)
+    print(f"Joueur {numero_joueur}: {representation_cartes}") 
+
+
+    
