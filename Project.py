5258bcb91602debffb0723974123fcd067d7cdcd
ashley <ashley.roper@ecam.fr>
2023-11-29 16:07:05 +0100

    ashley2

--- Project.py
+++ Project.py
@@ -1,51 +1,147 @@
 import random
 
-def display_rules(language):
-    if language == 'English':
-        print("Belote is a 32-card trick-taking game played in pairs. The goal is to win tricks with high-value cards and gather points.")
-    elif language == 'French':
-        print("La Belote est un jeu de 32 cartes joué en équipes. Le but est de remporter des plis avec des cartes de grande valeur et de collecter des points.")
+# Function to display rules in English
+def display_rules_en():
+    rules_en = """
+    Belote Rules:
+    - The game is played with 32 cards.
+    - Players form two teams.
+    - The goal is to win tricks containing valuable cards.
+    - ... (add more rules)
+    """
+    print(rules_en)
 
-def play_with_ai():
-    # Logic to play against AI
-    pass
+# Function to display rules in French
+def display_rules_fr():
+    rules_fr = """
+    Règles de la Belote :
+    - Le jeu se joue avec 32 cartes.
+    - Les joueurs forment deux équipes.
+    - Le but est de remporter des plis contenant des cartes de valeur.
+    - ... (ajoutez plus de règles)
+    """
+    print(rules_fr)
 
-def play_multiplayer(num_players):
-    # Logic for multiplayer game
-    pass
+# Function to get player names and shuffle order
+def get_player_names(num_players):
+    players = []
+    for i in range(num_players):
+        name = input(f"Enter player {i+1} name: ")
+        players.append(name)
 
-def start_game():
+    random.shuffle(players)
+    return players
+
+# Function to initialize and distribute initial cards (5 cards per player) and a trump card
+def distribute_initial_cards():
+    valeurs = ["7", "8", "9", "10", "Valet", "Dame", "Roi", "As"]
+    couleurs = ["[♥]", "[♦]","[♣]","[♠]"]
+    cartes = [(v, c) for v in valeurs for c in couleurs]
+
+    random.shuffle(cartes)
+
+    players_cards = [[] for _ in range(4)]
+    cards_dealt = 0
+
+    while cards_dealt < 20:  # 5 cards per player
+        for i in range(4):
+            if cards_dealt >= 20 or len(players_cards[i]) >= 5:
+                break
+            players_cards[i].append(cartes[cards_dealt])
+            cards_dealt += 1
+
+    return players_cards, cartes[cards_dealt:]  # Return dealt cards and remaining cards
+
+# Function to select a trump card
+def select_trump_card(remaining_cards):
+    random.shuffle(remaining_cards)
+    return remaining_cards[0]
+
+# Function to start the game
+def start_belote():
     print("Welcome to Belote!")
-    print("Choose your language: ")
-    print("1. English")
-    print("2. French")
-    language_choice = input("Enter your choice (1/2): ")
+    lang_choice = input("Choose 'F' for français or 'E' for english ").upper()
 
-    if language_choice == '1':
-        language = 'English'
-    elif language_choice == '2':
-        language = 'French'
+    if lang_choice == "E":
+        num_players = int(input("Enter number of players (2-4): "))
+        display_rules_en()
+    elif lang_choice == "F":
+        num_players = int(input("Entrer un nombre de joueur compris (2-4): "))
+        display_rules_fr()
     else:
-        print("Invalid choice. Defaulting to English.")
-        language = 'English'
+        print("Invalid choice / Choix invalide")
+        return
 
-    display_rules(language)
+    if 2 <= num_players <= 4:
+        players = get_player_names(num_players)
+        print("Starting multiplayer game with players:", players)
+        dealt_initial_cards, remaining_cards = distribute_initial_cards()
 
-    print("How many players? (1-4)")
-    num_players = int(input("Enter number of players: "))
+        for i in range(num_players):
+            representation_cards = ""
+            for value, suit in dealt_initial_cards[i]:
+                representation_cards += f'{value} of {suit}, '
+            representation_cards = representation_cards[:-2]
+            print(f"{players[i]}: {representation_cards}")
 
-    if num_players == 1:
-        print("You chose to play against the AI.")
-        play_with_ai()
-    elif 2 <= num_players <= 4:
-        print("You chose to play multiplayer.")
-        play_multiplayer(num_players)
+        # Select and display the trump card
+        trump_card = select_trump_card(remaining_cards)
+        print(f"The trump card is: {trump_card[0]} of {trump_card[1]}")
+
+        # Loop to offer players the chance to "take" the trump card or pass
+        taker = None
+        for i in range(len(players)):
+            response = input(f"{players[i]}, do you want to take the trump card? (Y/N): ").upper()
+            if response == "Y":
+                taker = players[i]
+                break
+
+        if taker:
+            print(f"{taker} took the trump card!")
+
+            # Players receive additional cards after the trump is taken
+            additional_cards = remaining_cards[:5]
+            remaining_cards = remaining_cards[5:]
+
+            # Distribute additional cards (2 to taker, 3 to others)
+            for i in range(num_players):
+                if players[i] == taker:
+                    dealt_initial_cards[i] += additional_cards[:3]
+                else:
+                    dealt_initial_cards[i] += additional_cards[2:5]
+
+            # Display updated hands
+            print("Distributing additional cards...")
+            for i in range(num_players):
+                representation_cards = ""
+                for value, suit in dealt_initial_cards[i]:
+                    representation_cards += f'{value} of {suit}, '
+                representation_cards = representation_cards[:-2]
+                print(f"{players[i]}: {representation_cards}")
+
+        else:
+            print("No one took the trump card. Redistributing cards...")
+
+            # Redistribute cards if no one takes the trump card
+            remaining_cards.extend([card for hand in dealt_initial_cards for card in hand])
+            random.shuffle(remaining_cards)
+
+            # Display redistributed cards
+            print("Redistributed cards:")
+            for i in range(num_players):
+                representation_cards = ""
+                for value, suit in remaining_cards[i * 8: (i + 1) * 8]:
+                    representation_cards += f'{value} of {suit}, '
+                representation_cards = representation_cards[:-2]
+                print(f"{players[i]}: {representation_cards}")
+
+    elif num_players == 1:
+        game_mode = "2"  # Set to play against AI automatically if only one player
+        players = get_player_names(1)  # Set 1 player for AI
+        print("Starting game against AI with player:", players[0])
+        # Implement AI game logic here
     else:
-        print("Invalid number of players. Please choose between 1 and 4.")
+        print("Invalid choice. Exiting.")
 
 # Start the game
-start_game()
-
-
-
-
+start_belote()
